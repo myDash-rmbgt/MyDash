56e695ce92db234a048a37bf37c605e9fb9c31cb
myDash-rmbgt <61596807+myDash-rmbgt@users.noreply.github.com>
2020-02-28 20:56:06 +0100

    Update config.inc.php

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -8,7 +8,7 @@
  
 //Tragt hier eure Verbindungsdaten zur Datenbank ein
 $db_host = 'localhost';
-$db_name = 'rmbgt';
-$db_user = 'rmbgt';
-$db_password = 'schule33330';
+$db_name = 'xxxx';
+$db_user = 'xxxx';
+$db_password = 'xxxx';
 $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
